95a465f289d366fa9b519eb79251923fe3e2d26b
Konrad Beckmann <konrad.beckmann@gmail.com>
2020-04-19 01:11:57 +0200

    mimxrt101x: Add linker script for running from RAM

--- hw/chip/mimxrt10xx/mimxrt101x/MIMXRT1011DAE5A.mk
+++ hw/chip/mimxrt10xx/mimxrt101x/MIMXRT1011DAE5A.mk
@@ -4,9 +4,14 @@ include hw/chip/$(TUF2_CHIP_FAMILY)/family.mk
 
 CFLAGS += -DCPU_MIMXRT1011DAE5A
 
+ifeq ($(VARIANT), ram)
+	LD_FILE = ../../hw/chip/$(TUF2_CHIP_FAMILY)/$(TUF2_CHIP_MEMBER)/MIMXRT1011xxxxx_ram.ld
+else
+	LD_FILE = ../../hw/chip/$(TUF2_CHIP_FAMILY)/$(TUF2_CHIP_MEMBER)/MIMXRT1011xxxxx_flexspi_nor.ld
+endif
+
 SRC_C += $(MCU_DIR)/system_MIMXRT1011.c
 
 SRC_S += $(MCU_DIR)/gcc/startup_MIMXRT1011.S
 
-LD_FILE = ../../hw/chip/$(TUF2_CHIP_FAMILY)/$(TUF2_CHIP_MEMBER)/MIMXRT1011xxxxx_flexspi_nor.ld
 
